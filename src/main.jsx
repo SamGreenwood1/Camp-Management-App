99f216c824eebb5411f52eec9ede243eed92107d
Sam Greenwood <jammer2001@gmail.com>
2025-07-18 12:01:58 -0400

    update

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ScheduleDisplay from './components/ScheduleDisplay';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScheduleDisplay />
+  </React.StrictMode>
+); 
