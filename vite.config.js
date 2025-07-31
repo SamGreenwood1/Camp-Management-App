05d7040d365502ddffe1cf523be08ebdaf306072
Sam Greenwood <git@samgreenwood.ca>
2025-07-31 16:46:52 -0400

    fix vite conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true
+  }
+})
+
   plugins: [react()],
-});
